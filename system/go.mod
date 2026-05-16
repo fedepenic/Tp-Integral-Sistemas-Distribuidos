ff437e8524f7052732a89ed659728dc3b2b65ce4
fedepenic <fpenic@fi.uba.ar>
2026-05-15 23:41:46 -0300

    Add RabbitMQ middleware package from tp-coordinacion
    Brings in internal/middleware with QueueMiddleware and ExchangeMiddleware,
    both implementing the Middleware interface (StartConsuming, StopConsuming,
    Send, Close). Factory functions provided for both variants.

--- system/go.mod
+++ system/go.mod
@@ -1,3 +1,5 @@
 module github.com/fedepenic/Tp-Integral-Sistemas-Distribuidos/system
 
 go 1.21
+
+require github.com/rabbitmq/amqp091-go v1.11.0
---
+++ system/internal/middleware/factory.go
@@ -0,0 +1,9 @@
+package middleware
+
+func CreateQueueMiddleware(queueName string, connectionSettings ConnSettings) (Middleware, error) {
+	return NewQueueMiddleware(queueName, connectionSettings)
+}
+
+func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings ConnSettings) (Middleware, error) {
+	return NewExchangeMiddleware(exchange, keys, connectionSettings)
+}
